df4224ce518fecb04a78c6c290ea8632d89b912b
Adithya <53051177+adithya-ananth@users.noreply.github.com>
2024-01-25 23:35:01 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home () {
     return (
         <div className="w-screen h-screen bg-pink-500 flex items-center justify-center">
             <div className="flex flex-col items-center space-y-2">
-                <img src="https://i.giphy.com/cLS1cfxvGOPVpf9g3y.webp"/>
+                <img src="https://i.giphy.com/cLS1cfxvGOPVpf9g3y.webp" className="object-cover h-80"/>
                 <p className="font-bold text-xl text-white">Will you be my valentine?</p>
                 <Button onClick={handleClick}>Yes!</Button>
                 <Button onMouseEnter={handleHover}>No</Button>
